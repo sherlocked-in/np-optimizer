0a3fdb1011a94aaef2059e46ecb123ea1e89ec71
sherlocked-in <parthvi.vivekiyer@gmail.com>
2026-01-09 19:21:24 -0500

    Update science_fair_model.py

--- science_fair_model.py
+++ science_fair_model.py
@@ -139,9 +139,10 @@ st.markdown("---")
 st.markdown("### 📚 **VALIDATED BY PUBLISHED STUDIES**")
 st.info("""
 **Dual-Transcytosis Precedents:**
-• T7/NGR nanoparticles: 5x brain delivery [ACS Appl Mater 2018]
-• AP1-NP-DOX: Best tumor kill [Oncotarget 2017]  
-• SS31-DOX: Extended survival [Int J Oncol 2025]
+
+**1. T7/NGR nanoparticles: 5x brain delivery [ACS Appl Mater 2018]**
+**2. AP1-NP-DOX: Best tumor kill [Oncotarget 2017]** 
+**3. SS31-DOX: Extended survival [Int J Oncol 2025]**
 
 **Cationic Toxicity Confirmed:**
 
